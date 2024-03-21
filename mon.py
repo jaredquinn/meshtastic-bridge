348f3b5cb7ba6e4cffac94cd6e8e7a173a617ec1
Jared Quinn <jared@river.catslair>
2024-03-21 14:03:29 +1100

    Add telemtry data from nodes (Channel & TX Utilisation)

--- mon.py
+++ mon.py
@@ -7,7 +7,7 @@ import logging
 import time
 import random
 
-from prometheus_client import start_http_server, Summary, Counter
+from prometheus_client import start_http_server, Summary, Counter, Gauge
 
 from pubsub import pub
 
@@ -20,8 +20,12 @@ interface = meshtastic.tcp_interface.TCPInterface(hostname="localhost")
 
 RADIO_PACKETS_PORT = Counter('radio_packets_app', 'Radio Packets by App', ['portnum'])
 RADIO_PACKETS_SENDER = Counter('radio_packets_sender', 'Radio Packets by Sender', ['sender'])
+
 LOCAL_PACKETS_PORT = Counter('local_packets_app', 'Local Packets by App', ['portnum'])
 LOCAL_PACKETS_SENDER = Counter('local_packets_sender', 'Local Packets by Sender', ['sender'])
+
+NODE_CH_UTILISATION = Gauge('node_channel_util', 'Node Telemetry Channel Utilisation', ['sender'])
+NODE_AIRTX_UTILISATION = Gauge('node_airtx_util', 'Node Telemetry Air TX Utilisation', ['sender'])
 
 DATA = {}
 
@@ -30,16 +34,16 @@ def onConnection(interface, topic=pub.AUTO_TOPIC):
     logger.info(f"Connected to node: userId={nodeInfo['user']['id']} hwModel={nodeInfo['user']['hwModel']}")
 
 
-def onReceive(packet, interface): # called when a packet arrives
+def onReceive(packet, interface):
     sender = packet['fromId']
     to = packet.get('toId')
     decoded = packet.get('decoded')
     port = decoded.get('portnum')
 
-    print(f"\n\n====== {port} <<< {sender} >>> {to}")
+    logger.info(f"\n\n====== {port} <<< {sender} >>> {to}")
     if port == 'SIMULATOR_APP':
         ins = decoded.get('simulator').get('portnum')
-        print(f'Simulator Packet {ins}')
+        logger.debug(f'Simulator Packet {ins}')
         pack = decoded.get('simulator')
 
         RADIO_PACKETS_PORT.labels(ins).inc()
@@ -49,32 +53,34 @@ def onReceive(packet, interface): # called when a packet arrives
         pack = decoded
 
         if sender in interface.nodes:
-            print(f"FROM: {interface.nodes[sender]['user']['shortName']} {interface.nodes[sender]['user']['longName']}")
+            logger.debug(f"FROM: {interface.nodes[sender]['user']['shortName']} {interface.nodes[sender]['user']['longName']}")
 
-        if 'X%s' % sender not in DATA:
-            DATA['X%s' % sender] = {}
-
+        if '%s' % sender not in DATA:
+            DATA['%s' % sender] = {}
 
         if port == 'POSITION_APP':
-            print('Updated Position for %s' % sender)
-            DATA['X%s' % sender]['position'] = pack.get('position')
+            logger.debug(f'Updated Position for {sender}')
+            #DATA['%s' % sender]['position'] = pack.get('position')
             pprint.pprint(pack.get('position'))
 
         if port == 'TELEMETRY_APP':
-            print('Updated Telemetry for %s' % sender)
-            DATA['X%s' % sender]['telemetry'] = pack.get('telemetry')
-            pprint.pprint(pack.get('telemetry'))
+            logger.debug('Updated Telemetry for %s' % sender)
+            telem = pack.get('telemetry')
+            pprint.pprint(telem)
+            if 'deviceMetrics' in telem:
+                NODE_CH_UTILISATION.labels(sender).set( telem['deviceMetrics'].get('channelUtilization') )
+                NODE_AIRTX_UTILISATION.labels(sender).set( telem['deviceMetrics'].get('airUtilTx' ))
 
         if port == 'NODEINFO_APP':
-            print('Updated Nodeinfo for %s' % sender)
-            DATA['X%s' % sender]['user'] = pack.get('user')
-            pprint.pprint(pack.get('user'))
+            logger.debug('Updated Nodeinfo for %s' % sender)
+            #DATA['%s' % sender]['user'] = pack.get('user')
+            #pprint.pprint(pack.get('user'))
 
         if port == 'TEXTMESSAGE_APP':
-            print('TEXT MESSAGE: %s' % pack)
+            logger.debug('TEXT MESSAGE: %s' % pack)
             pprint.pprint(pack)
 
-            #if 'X%s' % sender in DATA:
+            #if '%s' % sender in DATA:
             #    #pprint.pprint(DATA['X%s' % sender])
             print('------')
 
