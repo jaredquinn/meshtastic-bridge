9a63aabc5bc74b3f784b14cf07e7078586adb4e3
Jared Quinn <jared@river.catslair>
2024-03-27 12:22:10 +1100

    Clean up sending APRS before connection

--- plugin/aprs.py
+++ plugin/aprs.py
@@ -16,7 +16,12 @@ APRS_PASSWORD=os.environ.get('APRS_PASSWORD')
 APRS_HOST='rotate.aprs2.net'
 APRS_PORT=14580
 
-def integerToDMS(val, postfix):
+APRS_TEXT='MQTT,ANZ,LongFast'
+
+def integerToDMh(val, postfix):
+    """
+    Convert decimal long/lat to Deg Minutes Hundreths
+    """
     valDeg = int(val)
     valMin = (val-valDeg)*60
     valHth = int((valMin-int(valMin))*100)
@@ -24,15 +29,25 @@ def integerToDMS(val, postfix):
     return "%d%02d.%02d%1s" % (valDeg, valMin, valHth, postfix)
 
 def convertPosition(lat, lng):
+    """
+    Convert decimal position to APRS format
+    """
     lngS = 'E' if lng > 0 else 'W'
-    return integerToDMS(abs(lat), 'N' if lat > 0 else 'S') + '/' +  \
-             integerToDMS(abs(lng), 'E' if lng > 0 else 'W')
+    return integerToDMh(abs(lat), 'N' if lat > 0 else 'S') + '/' +  \
+             integerToDMh(abs(lng), 'E' if lng > 0 else 'W')
 
 class APRS_Plugin:
 
     def __init__(self):
         self._aprs = None
-        logging.info('Plugin Initialized')
+        self._enabled = False
+        self._aprs = aprslib.IS(APRS_CALLSIGN,
+                      passwd=APRS_PASSWORD,
+                      host=APRS_HOST,
+                      port=APRS_PORT,
+                      skip_login=False)
+
+        logger.info('Plugin Initialized')
 
     def handle_POSITION_APP(self, sender, fullpacket, interface=None):
 
@@ -42,45 +57,46 @@ class APRS_Plugin:
 
     def process_position(self, sender, pos, interface):
 
+        if not self._enabled:
+            logger.warn("Not sending packet due to no APRS-IS connection")
+            return
+
         if APRS_CALLSIGN is None:
             return
 
         if 'latitudeI' not in pos or 'longitudeI' not in pos:
-            logging.warn("Incomplete position in packet, not sending to APRS")
+            logger.warn("Incomplete position in packet, not sending to APRS")
             return
 
         if sender is None:
-            logging.warn("No Sender in this packet, not sending to APRS")
+            logger.warn("No Sender in this packet, not sending to APRS")
             return
 
         node = interface.nodes[sender]
         if not 'user' in node:
-            logging.warn("No User in interfaces yet, not sending to APRS")
+            logger.warn("No User in interfaces yet, not sending to APRS")
             return
 
+        # user: {'id': '!da56df6c', 'longName': 'Bray Park 1', 'shortName': 'BPK1', 'macaddr': 'NLfaVt9s', 'hwModel': 'HELTEC_V3'}
         user = interface.nodes[sender].get('user')
         short = user.get('shortName')
         if short is None:
-            logging.warn("Node has no shortname, not sending to APRS")
+            logger.warn("Node has no shortname, not sending to APRS")
 
         long = user.get('longName')
-
-        #logging.warn(f"Sender {sender}")
-        #logging.warn(f"Node {node}")
-
-        sender = sender.replace('!', 'M')
         lat = pos.get('latitude',0)
         lng = pos.get('longitude',0)
-        logging.warn(f"Lat {lat} Long {lng}")
         pos = convertPosition(lat,lng)
+        logger.debug(f"Lat {lat} Long {lng} = {pos}")
+
         now = datetime.now(timezone.utc)
         ds = now.strftime("%d%H%Mz")
-        #logging.warn(short)
+        hardware = user.get('hwModel')
 
         station = "MESH-%s" % short.replace(' ', '')
-        MESSAGE=f"{station:<9}*{ds}{pos}nMestastic Node {short}/{long}, LongFast, ANZ"
+        MESSAGE=f"{station:<9}*{ds}{pos}nMestastic Node {short}/{long}, {APRS_TEXT}. Device ID {sender} ({hardware})."
         PACKET=f"{APRS_CALLSIGN}>APDW16,WIDE1-1:;{MESSAGE}"
-        logging.warn(f"SENDING APRS: {PACKET}")
+        logger.info(f"TX: {PACKET}")
         self._aprs.sendall(PACKET)
 
     def start(self, interface=None):
@@ -88,18 +104,14 @@ class APRS_Plugin:
             logger.warn("Not setting up APRS, Variable not set")
             return
 
-        self._aprs = aprslib.IS(APRS_CALLSIGN,
-                      passwd=APRS_PASSWORD,
-                      host=APRS_HOST,
-                      port=APRS_PORT,
-                      skip_login=False)
-
         self._aprs.connect(blocking=True)
+        self._enabled = True
 
+        # @TODO: bacon on startup and regularly on timer
         #now = datetime.now(timezone.utc)
         #ds = now.strftime("%d%H%Mz")
         #MESSAGE=f">{ds}I am a robot"
         #PACKET=f"{APRS_CALLSIGN}>APDW16,WIDE1-1:;{MESSAGE}"
-        #logging.warn(f'Sending {PACKET}')
+        #logger.warn(f'Sending {PACKET}')
         #self._aprs.sendall(PACKET)
 
