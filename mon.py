5b1f790186a79ea8b5e99a49b093bf306380ee8c
Jared Quinn <jared@jaredquinn.info>
2024-04-02 11:04:19 +1100

    Exit gracefully on disconnect

--- mon.py
+++ mon.py
@@ -21,6 +21,7 @@ import logging
 import time
 import random
 import os
+import sys
 
 import meshtastic
 import meshtastic.tcp_interface
@@ -33,16 +34,22 @@ import plugin.mqtt
 import plugin.message_logger
 import plugin.aprs
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format="%(asctime)s %(levelname)-8s: %(message)s",
+                    level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 from plugin import call_plugin_function, register_plugin
+
+MESHTASTIC_HOST=os.environ.get("MESHTASTIC_HOST", 'localhost')
 
 register_plugin('prometheus.Prometheus_Plugin')
 register_plugin('message_logger.MessageLogger_Plugin')
 register_plugin('aprs.APRS_Plugin')
 register_plugin('mqtt.MQTT_Plugin')
 
+def OnMeshDisconnect(interface):
+    sys.exit("Connection Lost to Meshtastic Node")
 
 def OnMeshConnection(interface, topic=pub.AUTO_TOPIC):
     nodeInfo = interface.getMyNodeInfo()
@@ -57,42 +64,47 @@ def OnMeshReceive(packet, interface):
     sender = packet.get('fromId', packet.get('from'))
     to = packet.get('toId')
     ch = packet.get('channel', None)
+    dch = "Default" if ch is None else ch
 
     decoded = packet.get('decoded')
     port = decoded.get('portnum')
 
     if port == 'SIMULATOR_APP':
         ins = decoded.get('simulator',{}).get('portnum')
-        logger.info(f"SIMPKT: {ch}/{ins} <<< {sender} >>> {to}")
+        logger.info(f"SIM {dch}/{ins} <<< {sender} >>> {to}")
         call_plugin_function('count_packets', interface, 'SIM', sender, ins)
 
     else:
         frm = "(No Details)"
         if sender in interface.nodes:
             frm = f"({interface.nodes[sender]['user']['shortName']} {interface.nodes[sender]['user']['longName']})"
-        logger.info(f"LCLPKT: {ch}/{port} <<< {sender} {frm} >>> {to}")
-        logger.debug(f"LCLPKT: {packet}")
+
+
+        logger.info(f"LCL {dch}/{port} <<< {sender} {frm} >>> {to}")
+        logger.debug(f"LCL {packet}")
 
         call_plugin_function(f'handle_{port}', interface, sender, packet)
         call_plugin_function('count_packets', interface, 'LOCAL', sender, port)
 
-
-if __name__ == '__main__':
-    MESHTASTIC_HOST=os.environ.get("MESHTASTIC_HOST", 'localhost')
-
-    pub.subscribe(OnMeshReceive, "meshtastic.receive")
-    pub.subscribe(OnMeshConnection, "meshtastic.connection.established")
+def connect_mesh():
     logger.info(f"Conneting to {MESHTASTIC_HOST}")
-
     try:
         interface = meshtastic.tcp_interface.TCPInterface(hostname=MESHTASTIC_HOST)
     except:
-        logger.error(f'Unable to connect to Meshtastic Node {MESHTASTIC_HOST}')
+        sys.exit(f'Unable to connect to Meshtastic Node {MESHTASTIC_HOST}')
+    return interface
 
+
+if __name__ == '__main__':
+
+    pub.subscribe(OnMeshReceive, "meshtastic.receive")
+    pub.subscribe(OnMeshConnection, "meshtastic.connection.established")
+    pub.subscribe(OnMeshDisconnect, "meshtastic.meshtastic.connection.lost")
+
+    interface=connect_mesh()
     call_plugin_function('start', interface)
 
     while True:
         time.sleep(1)
         call_plugin_function('loop', interface)
-
 
