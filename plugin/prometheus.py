3bbb8d0da8648d94d1fd5e8fbe35b91dc6fb3cfa
Jared Quinn <jared@jaredquinn.info>
2024-04-03 15:12:38 +1100

    Add node_seen and _2h metrics (see README)

--- plugin/prometheus.py
+++ plugin/prometheus.py
@@ -26,8 +26,7 @@ __PLUGIN_NAME__ = 'prometheus'
 
 
 PROMETHEUS_PORT=os.environ.get('PROMETHEUS_PORT', None)
-
-UPDATE_SECONDS = 30
+PROMETHEUS_UPDATE_SECONDS = 10
 
 METRICS = {
   'SIM_PACKETS_PORT': Counter('sim_packets_app', 'Sim Packets by App', ['portnum']),
@@ -37,8 +36,13 @@ METRICS = {
 
   'NODE_CH_UTILISATION': Gauge('node_channel_util', 'Node Telemetry Channel Utilisation', ['sender']),
   'NODE_AIRTX_UTILISATION': Gauge('node_airtx_util', 'Node Telemetry Air TX Utilisation', ['sender']),
-  'NODE_COUNT': Gauge('node_count', 'Number of Nodes seen in last 10 minutes'),
+
+  'NODE_COUNT': Gauge('node_count', 'Number of Nodes seen in last 5 minutes (lastseen in Meshtastic)'),
+  'NODE_COUNT_2HR': Gauge('node_count_2h', 'Number of Nodes seen in last 2 hours (lastseen in Meshtastic)'),
+  'NODE_SEEN': Gauge('node_seen', 'Number of Nodes that we saw traffic from in last 5 minutes'),
+  'NODE_SEEN_2HR': Gauge('node_seen_2h', 'Number of Nodes that we saw traffic from in last 2 hours'),
 }
+
 
 def getTimeAgo(ts):
     """Format how long ago have we heard from this node (aka timeago)."""
@@ -47,14 +51,16 @@ def getTimeAgo(ts):
 class Prometheus_Plugin:
 
     def __init__(self):
+        self.lastseen = {}
+        self._count = 0
+        self.active = False
+
         if PROMETHEUS_PORT is None:
             logger.warn('No PROMETHEUS_PORT specified.  Not collecting metrics.')
 
-        self._count = 0
 
     def handle_TELEMETRY_APP(self, sender, fullpacket, interface=None):
-        if PROMETHEUS_PORT is None:
-            return
+        if not self.active: return
 
         packet = fullpacket.get('decoded')
         telem = packet.get('telemetry', {})
@@ -66,42 +72,77 @@ class Prometheus_Plugin:
                 METRICS['NODE_CH_UTILISATION'].labels(sender).set( telem['deviceMetrics'].get('channelUtilization',0) )
 
     def update_nodecount(self, interface=None):
-        if PROMETHEUS_PORT is None:
-            return
+        """
+        Update nodecount using the meshtastic nodedb lastheard time
+        """
+        if not self.active: return
 
         logger.debug('Updating Node Count')
-        nodes = []
+        count2h = count = 0
         for node in interface.nodes.values():
-            r = {}
-            r['user'] = node.get('user')
+            #print(node)
             seconds = getTimeAgo(node.get("lastHeard"))
             if seconds is not None:
                 if seconds.total_seconds() < 600:
-                    r['since'] = seconds
-                    nodes.append(node)
+                    count = count + 1
+                if seconds.total_seconds() < 7200:
+                    count2h = count2h + 1
 
-        logger.info("%d total, %d active" % (len(interface.nodes.values()), len(nodes)))
-        METRICS['NODE_COUNT'].set(len(nodes))
+        METRICS['NODE_COUNT'].set(count)
+        METRICS['NODE_COUNT_2HR'].set(count2h)
+        logger.info("Meshtastic (node_count): %d total, %d active (5m) %d active (2h)" % \
+                (len(interface.nodes.values()), count, count2h))
+
+    def update_nodecount_internal(self, interface=None):
+        """
+        Update nodecount using the internal lastheard time
+        """
+        if not self.active: return
+
+        total = nodecount = nc2h = 0
+        for n,v in self.lastseen.items():
+            delta = datetime.now() - v 
+            if delta.total_seconds() < 600:
+                nodecount = nodecount + 1
+            if delta.total_seconds() < 7200:
+                nc2h = nc2h + 1
+            total = total + 1
+
+        METRICS['NODE_SEEN'].set(nodecount)
+        logger.info("Packets Seen (node_seen): %d total, %d active (5m) %d active (2h)" % \
+                (total, nodecount, nc2h))
+
 
     def count_packets(self, prefix, sender, port, interface=None):
-        if PROMETHEUS_PORT is None:
-            return
+        if not self.active: return
 
         logger.debug('Updating Packet Counts')
         METRICS[f'{prefix}_PACKETS_PORT'].labels(port).inc()
         METRICS[f'{prefix}_PACKETS_SENDER'].labels(sender).inc()
 
+        self.lastseen[sender] = datetime.now()
+
     def start(self, interface=None):
+
         if PROMETHEUS_PORT is not None:
             logger.info(f'Starting Prometheus Server.  Listening on {PROMETHEUS_PORT}')
             start_http_server(int(PROMETHEUS_PORT))
 
+            for node in interface.nodes.values():
+                user = node.get('user', {})
+                lh = node.get('lastHeard')
+                if lh:
+                    self.lastseen[user.get('id')] = datetime.fromtimestamp(node.get('lastHeard'))
+
+            self.active = True
+
     def loop(self, interface=None):
-        if PROMETHEUS_PORT is None:
-            return
+        if not self.active: return
 
         self._count = self._count + 1
-        if self._count > UPDATE_SECONDS:
+        if self._count >= PROMETHEUS_UPDATE_SECONDS:
             self.update_nodecount(interface)
+            self.update_nodecount_internal(interface)
             self._count = 0
+
 
